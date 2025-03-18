7e581ed5f661e36d599fed412c749ce778e41935
now2ah <hhw3287@hanmail.net>
2025-03-18 09:39:34 +0900

    message network

---
+++ 0318NetworkClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace _0318NetworkClient
+{
+    public class Message
+    {
+        public string message;
+        public Message(string msg)
+        {
+            message = msg;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageClientProcess();
+        }
+
+        static void MessageClientProcess()
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 4000);
+
+            serverSocket.Connect(endPoint);
+
+            string message = "안녕하세요";
+            Message msg = new Message(message);
+
+            byte[] buffer = new byte[1024];
+
+            buffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
+
+            serverSocket.Send(buffer);
+
+            byte[] receiveBuffer = new byte[1024];
+            serverSocket.Receive(receiveBuffer);
+
+            string receiveString = Encoding.UTF8.GetString(receiveBuffer);
+            //Message receiveMessage = JsonConvert.DeserializeObject<Message>(receiveString);
+
+            Console.WriteLine(receiveString);
+
+            serverSocket.Close();
+        }
+
+        static void FileClientProcess()
+        {
+
+        }
+    }
+}
--- 0318NetworkProject/Program.cs
+++ 0318NetworkProject/Program.cs
@@ -3,13 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace _0318NetworkProject
 {
+    public class Message
+    {
+        public string message;
+        public Message(string msg)
+        {
+            message = msg;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            MessageServerProcess();
+        }
+
+        static void MessageServerProcess()
+        {
+            int backlog = 10;
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 4000);
+
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(backlog);
+
+            Socket clientSocket = listenSocket.Accept();
+
+            byte[] buffer = new byte[1024];
+            clientSocket.Receive(buffer);
+
+            string receiveString = Encoding.UTF8.GetString(buffer);
+            //Message message = JsonConvert.DeserializeObject<Message>(receiveString);
+
+            Console.WriteLine(receiveString);
+
+            string sendMessage = "반가워요";
+            Message msg = new Message(sendMessage);
+
+            byte[] sendBuffer = new byte[1024];
+            sendBuffer = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg));
+
+            clientSocket.Send(sendBuffer);
+
+            clientSocket.Close();
+            listenSocket.Close();
+        }
+
+        static void FileServerProcess()
+        {
+            int backlog = 10;
+
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 4000);
+
+            listenSocket.Bind(endPoint);
+            listenSocket.Listen(backlog);
+
+            Socket clientSocket = listenSocket.Accept();
+
+            byte[] buffer = new byte[1024];
         }
     }
 }
